ece9b4f214cd2a0845ec73be4fdcb212766242dc
dsyrstad <dsyrstad@b352c31b-9b19-0410-8aa3-4501cdae6922>
2008-04-23 02:31:55 +0000

    Sketching out

--- src/main/java/Tuple.java
+++ src/main/java/Tuple.java
@@ -1,42 +1,47 @@
 import java.util.ArrayList;
 import java.util.List;
 
-
-
 public class Tuple
 {
     private List<Atom> atoms;
-    
-    public Tuple(Object... atomValues) {
+
+    public Tuple(Object... atomValues)
+    {
         assert atomValues != null;
-        
+
         atoms = new ArrayList<Atom>(atomValues.length);
         for (Object atomValue : atomValues) {
             atoms.add(new Atom(atomValue));
         }
     }
-    
-    public Atom get(int index) {
+
+    public Atom get(int index)
+    {
         return atoms.get(index);
     }
-    
-    public Atom get(String atomName) {
+
+    public Atom get(String atomName)
+    {
         return null;
     }
-    
-    public void set(int index, Atom atom) {
-        
+
+    public void set(int index, Atom atom)
+    {
+
     }
-    
-    public void set(String atomName, Atom atom) {
-        
+
+    public void set(String atomName, Atom atom)
+    {
+
     }
-    
-    public long getId() {
+
+    public long getId()
+    {
         return 0;
     }
-    
-    public Tuple getDomain() {
+
+    public Tuple getDomain()
+    {
         return null;
     }
 }
