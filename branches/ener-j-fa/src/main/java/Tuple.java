513655293433a7c4a4cf43f868aa3826283c1e58
dsyrstad <dsyrstad@b352c31b-9b19-0410-8aa3-4501cdae6922>
2008-04-23 02:31:26 +0000

    Sketching out

--- branches/ener-j-fa/src/main/java/Tuple.java
+++ branches/ener-j-fa/src/main/java/Tuple.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -5,4 +6,37 @@ import java.util.List;
 public class Tuple
 {
     private List<Atom> atoms;
+    
+    public Tuple(Object... atomValues) {
+        assert atomValues != null;
+        
+        atoms = new ArrayList<Atom>(atomValues.length);
+        for (Object atomValue : atomValues) {
+            atoms.add(new Atom(atomValue));
+        }
+    }
+    
+    public Atom get(int index) {
+        return atoms.get(index);
+    }
+    
+    public Atom get(String atomName) {
+        return null;
+    }
+    
+    public void set(int index, Atom atom) {
+        
+    }
+    
+    public void set(String atomName, Atom atom) {
+        
+    }
+    
+    public long getId() {
+        return 0;
+    }
+    
+    public Tuple getDomain() {
+        return null;
+    }
 }
