4e44a8e68a86bca40b86c1825a26b146a41b998b
dsyrstad <dsyrstad@b352c31b-9b19-0410-8aa3-4501cdae6922>
2008-04-23 00:46:50 +0000

    Sketching out

---
+++ branches/ener-j-fa/src/main/java/Atom.java
@@ -0,0 +1,6 @@
+
+public class Atom
+{
+    // String, long, decimal, bigint, boolean, double, byte[], Tuple, expression, null (==Empty tuple).
+    private Object value;
+}
---
+++ branches/ener-j-fa/src/main/java/Tuple.java
@@ -0,0 +1,8 @@
+import java.util.List;
+
+
+
+public class Tuple
+{
+    private List<Atom> atoms;
+}
